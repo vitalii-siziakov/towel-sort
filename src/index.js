7a3acd6555556e12fec93a35ed02c9e7fbfc497f
Vitalii Siziakov <vitalii.siziakov@gmail.com>
2021-07-09 16:19:26 +0000

    feat: implement towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
-// You should implement your task here.
+function minMaxSort (arr) {return arr.sort((a,b) => a - b)}; 
+function maxMinSort (arr) {return minMaxSort(arr).reverse()};
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  let resultArr = [];
+
+  if (arguments.length) {
+    matrix.forEach((element, i) => {
+      if ((i + 1) % 2 === 0) {
+        resultArr = resultArr.concat(maxMinSort(element.slice()));
+      } else {
+        resultArr = resultArr.concat(minMaxSort(element.slice()));
+      }
+    });
+  }
+
+  return resultArr;
+};
